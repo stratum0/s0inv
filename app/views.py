7289597639b82f231eb68faffdbdde2b9571f8c4
Chamaeleon <y0067315@tu-bs.de>
2017-10-15 11:45:39 +0200

    some repairs and new redirects

--- app/views.py
+++ app/views.py
@@ -1,12 +1,12 @@
 from flask import Flask, redirect, request, send_from_directory
-from app import app, inventory_labels
+from app import app, inventory_labels,db
 from flask_security import Security, login_required, login_user, logout_user, current_user
 
 
 @app.route("/")
 @login_required
 def app_index():
-    return "Hello World!"
+    return redirect("/admin")
 
 @app.route("/<int:item_id>")
 @login_required
@@ -19,6 +19,6 @@ def app_label():
     ids = request.args.get('ids', None)
     if ids:
         filename = inventory_labels.create_pdf(db, ids.split(','))
-        return send_from_directory('tmp', filename)
+        return send_from_directory('../tmp', filename)
     else:
         return "missing parameter: ids"
